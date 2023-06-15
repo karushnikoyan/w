a7c7ffbf215a963472b72b4ba3abb0c3829c3abf
RafJeyranyan <rafor1010@gmail.com>
2023-06-15 23:49:22 +0400

    fixes

--- lib/view/dummy/workout/exercises_screen.dart
+++ lib/view/dummy/workout/exercises_screen.dart
@@ -357,65 +357,67 @@ class _ExerciseDisplayScreenState extends State<ExerciseDisplayScreen> {
                     fit: BoxFit.cover),
 
                   ),
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 30.0,
-                      ),
-                      TextField(
-                        controller: nameController,
-                            style: TextStyle(color: Colors.white60),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 30.0,
+                        ),
+                        TextField(
+                          controller: nameController,
+                              style: TextStyle(color: Colors.white60),
+                          decoration: InputDecoration(
+                              fillColor: AppColors.cOrange,filled:true,
+                              hintText: "Title",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 2.0),
+                                  borderRadius: BorderRadius.circular(10.0))),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        TextField(
+                          controller: weightController,
+                          style: TextStyle(color: Colors.white60),
+
+                          decoration: InputDecoration(
+                              fillColor: AppColors.cOrange,filled:true,
+                              hintText: "Weight",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 2.0),
+                                  borderRadius: BorderRadius.circular(10.0))),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        TextField(
+                          controller: repsController,
+                          style: TextStyle(color: Colors.white60),
+
+                          decoration: InputDecoration(
+                              fillColor: AppColors.cOrange,filled:true,
+                              hintText: "Reps",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 2.0),
+                                  borderRadius: BorderRadius.circular(10.0))),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        TextField(
+                          controller: setsController,
+                          style: TextStyle(color: Colors.white60),
+
                         decoration: InputDecoration(
                             fillColor: AppColors.cOrange,filled:true,
-                            hintText: "Title",
+                            hintText: "Sets",
                             border: OutlineInputBorder(
                                 borderSide: BorderSide(width: 2.0),
                                 borderRadius: BorderRadius.circular(10.0))),
                       ),
-                      const SizedBox(
-                        height: 10.0,
-                      ),
-                      TextField(
-                        controller: weightController,
-                        style: TextStyle(color: Colors.white60),
-
-                        decoration: InputDecoration(
-                            fillColor: AppColors.cOrange,filled:true,
-                            hintText: "Weight",
-                            border: OutlineInputBorder(
-                                borderSide: BorderSide(width: 2.0),
-                                borderRadius: BorderRadius.circular(10.0))),
-                      ),
-                      const SizedBox(
-                        height: 10.0,
-                      ),
-                      TextField(
-                        controller: repsController,
-                        style: TextStyle(color: Colors.white60),
-
-                        decoration: InputDecoration(
-                            fillColor: AppColors.cOrange,filled:true,
-                            hintText: "Reps",
-                            border: OutlineInputBorder(
-                                borderSide: BorderSide(width: 2.0),
-                                borderRadius: BorderRadius.circular(10.0))),
-                      ),
-                      const SizedBox(
-                        height: 10.0,
-                      ),
-                      TextField(
-                        controller: setsController,
-                        style: TextStyle(color: Colors.white60),
-
-                      decoration: InputDecoration(
-                          fillColor: AppColors.cOrange,filled:true,
-                          hintText: "Sets",
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(width: 2.0),
-                              borderRadius: BorderRadius.circular(10.0))),
-                    ),
-                  ],
+                    ],
                 ),
+                  ),
               ),
             );
         });
